29bab7c122a83d01da9dd4d5749cb3792b39faf4
EkaterinaShumskaya <katya_vodich@mail.ru>
2022-12-30 13:13:56 +0300

    init

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,6 +27,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e)
         onChangeChecked?.(e.currentTarget.checked)
          // {e.currentTarget.value}
         // задачка на написание онченджа
